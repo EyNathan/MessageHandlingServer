c5954cdb3286f6ac29ccff937ac8f832ebc68c16
e . nathan <eyalnatan1@gmail.com>
2023-09-11 09:56:24 +0300

    My First Commit

---
+++ src/helloWorld.java
@@ -0,0 +1,7 @@
+public class helloWorld {
+    public static void main(String[] args){
+        System.out.println("Hello World");
+        System.out.println("Eyal Natan");
+        System.out.println(32 * 2);
+    }
+}
